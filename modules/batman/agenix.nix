37905500290463b0e77e2da12343cd3327c60834
ryan bas <ryanbas21@gmail.com>
2026-09-01 11:14:04 -0600

    chore: declarative-gpg-setup

--- modules/batman/agenix.nix
+++ modules/batman/agenix.nix
@@ -2,7 +2,7 @@
 { inputs, ... }:
 
 {
-  users.batman.home.pc = { config, ... }: {
+  users.batman.home.pc = { pkgs, config, ... }: {
     imports = [
       inputs.agenix.homeManagerModules.default
     ];
@@ -19,6 +19,12 @@
         file = ../../secrets/zai-api-key.age;
       };
     };
+    home.activation.importGpgKey = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+      if ! ${pkgs.gnupg}/bin/gpg --list-secret-keys F3EB6A9821002B2C >/dev/null 2>&1; then
+        ${pkgs.gnupg}/bin/gpg --import \
+          ${config.age.secrets.gpg-private-key.path}
+      fi
+    '';
   };
 }
 
